eaa55f298c2c2123310d1a260dcf385f7efca26e
buzzm <b_moschetti@yahoo.com>
2024-10-14 11:52:27 -0400

    more changes

--- webstart.sh
+++ webstart.sh
@@ -8,13 +8,16 @@ if [ "$OPENAI_API_KEY" == "" ] ; then
     printf "Set env var OPENAI_API_KEY\n"
     exit 1
 fi
-if [ "$REACT_APP_AMI_URL" == "" ] ; then
-    printf "Set env var REACT_APP_AMI_URL\n"
-    exit 1
-fi
+#if [ "$REACT_APP_AMI_URL" == "" ] ; then
+#    printf "Set env var REACT_APP_AMI_URL\n"
+#    exit 1
+#fi
 
 # REACT_APP_AMI_URL=https://localhost:5001
+
 # REACT_APP_AMI_URL=https://dynIP:8080
+# EIP  Elastic IP!
+#  	 50.112.191.129
 
 
 CWD=`pwd`
@@ -28,7 +31,7 @@ source venv.1/bin/activate
 (cd apps/amiserver ; nohup python3 -u app.py --api_key $OPENAI_API_KEY --ami_cpt ../../ami.cpt  --local_cpt ../../local.cpt --snippets ../../snippets.txt > "$CWD/amiserver.log" 2>&1 &)
 
 #  last is node:
-(cd apps/gui/ami-frontend/src ; PORT=3000 nohup npm start > "$CWD/node.log" 2>&1 &)
+(cd apps/gui/ami-frontend/src ; PORT=3000 REACT_APP_AMI_URL=https://dynIP:8080 nohup npm start > "$CWD/node.log" 2>&1 &)
 
 
 
